9cf88dd44c75b17ed6879554ede05422f6febd8d
Arvind Sharma <mail.arvind.s@gmail.com>
2022-02-04 19:22:35 +0000

    7th

--- lib/employee.rb
+++ lib/employee.rb
@@ -2,4 +2,18 @@ class Employee < ActiveRecord::Base
 
   belongs_to :store
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate_validation, presence: true
+  validates :store_id, presence: true
+
+  def hourly_rate_validation
+    if hourly_rate < 40 || hourly_rate > 200
+      error.add(:hourly_rate, "Not acceptable range")
+      false
+    else
+      true
+    end
+  end
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,27 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  
+  validates :name, presence: true, length: {
+    minimum: 3
+  }
+  validate :annual_revenue_validation
+  validate :apparel_validation
+  
+  def annual_revenue_validation
+    if annual_revenue.nil? || annual_revenue < 0
+      errors.add(:annual_revenue, " invalid annual_revenue")
+    end
+  end
+
+  def apparel_validation
+    if !mens_apparel && !womens_apparel
+      errors.add(:apparel, " need any apparel")
+    end
+  end
 
 end
+
+
+# Stores must always have a name that is a minimum of 3 characters
+# Stores have an annual_revenue that is a number (integer) that must be 0 or more
+# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
