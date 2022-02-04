4e64c19828ee56109af3a22d37af911ffa5281c0
Arvind Sharma <mail.arvind.s@gmail.com>
2022-02-04 09:13:27 +0000

    completed first excercise

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,25 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store1 = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+store2 = Store.new
+store2.name = 'Richmond'
+store2.annual_revenue = 1260000
+store2.mens_apparel = false
+store2.womens_apparel = true  
+store2.save
+
+store3 = Store.new
+store3.name = 'Gastown'
+store3.annual_revenue = 190000
+store3.mens_apparel = true
+store3.womens_apparel = false  
+store3.save
+
+
+# 1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
+#   * Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+#   * Richmond (annual_revenue of 1260000 carries women's apparel only)
+#   * Gastown (annual_revenue of 190000 carries men's apparel only)
+# 2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the database.
