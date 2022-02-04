7e148cc4ff2cca6fa8775c5bee021dfc38de9504
Arvind Sharma <mail.arvind.s@gmail.com>
2022-02-04 17:52:23 +0000

    4th exercise

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -8,6 +8,22 @@ puts "----------"
 
 # Your code goes here ...
 
+store3 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store4 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store5 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true) 
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@women_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+@women_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
 
 
 # Exercise 4: Loading a subset of stores
